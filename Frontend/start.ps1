bfcd417fc15947fa88c9c9ea5be637249f656ac7
5con <connorkulawiak@gmail.com>
2025-09-28 17:25:48 -0500

    Pwrs script change

--- Frontend/start.ps1
+++ Frontend/start.ps1
@@ -1,15 +1,11 @@
 try {
-    python --version | Out-Null
-    Write-Host "Using Python to start server..."
     python -m http.server 8000
 } catch {
     # If Python fails, try .NET
     try {
-        dotnet --version | Out-Null
-        Write-Host "Python not found, using .NET to start server..."
+        dotnet tool install --global dotnet-serve
         dotnet serve -p 8000
     } catch {
-        Write-Host "Neither Python nor .NET SDK is installed. Please install one of them to run the server."
         exit 1
     }
 }
